eee82fc0aaee4f6723f2b6a2b138f50be812ac23
wada <wada@avita.co.jp>
2022-07-25 20:11:46 +0900

    implement fibonacci by python & AWS CodeWhisperer

---
+++ main.py
@@ -0,0 +1,17 @@
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def main():
+    # execute fibonacci(5)
+    for i in range(0, 10):
+        print(fibonacci(i))
+
+
+if __name__ == "__main__":
+    main()
